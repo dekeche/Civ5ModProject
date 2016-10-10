e0bb5c6ca4e9ec16a3d4781c7e9e4688846d9d30
dekeche <jesse.mckinley@mymail.champlain.edu>
2016-10-10 15:53:41 -0400

    Tested lua script to change features.
    Idea: Create new features, duplicates of existing, that reduce food from tiles and grand production.
    Then, on tile culture change, if civ is necromancer, replace existing tile feature with new one.
    
    This process does work, but the replaced tile does not have a graphic.
    
    Possible alternatives:
    Create as improvement, place at all tiles.
    Dissadvantages is that

--- NecromancerCivMod/LUA/NecroTrait.lua
+++ NecromancerCivMod/LUA/NecroTrait.lua
@@ -7,15 +7,10 @@
 --Central per turn call for necro abilities
 --------------------------------------------------------------
 function NecroPerTurn(iPlayer)
-
-	print("NecroPerTurn");
 	local pPlayer = Players[iPlayer];
 	if pPlayer:IsAlive() then
-		print("IsAlive");
 		if not pPlayer:IsMinorCiv() and not pPlayer:IsBarbarian() then
-			print("Is Major Civ");
 			if pPlayer:GetCivilizationType()==GameInfoTypes["CIVILIZATION_NECRO"] then
-				print("Is Necro");
 				NecroPopulationGrowthLimiter(pPlayer);
 				NecroUnitPromotionCheck(pPlayer);
 			end
@@ -24,15 +19,11 @@ function NecroPerTurn(iPlayer)
 end
 GameEvents.PlayerDoTurn.Add(NecroPerTurn)
 
-
 --------------------------------------------------------------
 --Removes food from necro controlled cities
 --------------------------------------------------------------
 function NecroPopulationGrowthLimiter(player)
-
-	print("NecroPopulationGrowthLimiter")
 	for pCity in player:Cities() do
-	print("City Food Reset.")
 	pCity:SetFood(0);
 	end
 end
@@ -41,23 +32,16 @@ end
 --Handles unique unit promotions for necro
 --------------------------------------------------------------
 function NecroUnitPromotionCheck(player)
-	print("NecroUnitPromotionCheck");
 	local numCities = player:GetNumCities();
 	local id = GameInfo.UnitPromotions.PROMOTION_NECRO_RAISE_THE_DEAD.ID
 	if numCities > 0 then
-		print("Cities Valid");
 		for pUnit in player:Units() do
-			print("Check Unit");
 			if pUnit:IsHasPromotion(id) then
-				print("Unit has id");
-				print(numCities);
 				-- Reset promotion state,
 				--	-Lower level/experience
 				--	-Decrease experience, if needed.
 				local level = pUnit:GetLevel();
-				print(level);
 				local experience = pUnit:GetExperience();
-				print(experience);
 				pUnit:SetLevel(level-1);
 				pUnit:SetExperience(experience-10);
 				pUnit:SetHasPromotion(id,false);
@@ -81,3 +65,42 @@ function NecroUnitPromotionCheck(player)
 		end
 	end
 end
+
+--------------------------------------------------------------
+--Detect necro city expansion/creation, I assume.
+--Check tile, Check for features/Improvements
+--Change Features/Improvements as needed.
+--------------------------------------------------------------
+function NecroCorruptionSpread(hexX, hexY, player, unknown)	
+	local pPlayer = Players[player];
+	if pPlayer:IsAlive() then
+		print("IsAlive");
+		if not pPlayer:IsMinorCiv() and not pPlayer:IsBarbarian() then
+			print("Is Major Civ");
+			if pPlayer:GetCivilizationType()==GameInfoTypes["CIVILIZATION_NECRO"] then
+				print("Is Necro");
+				local gridPosX, gridPosY = ToGridFromHex( hexX, hexY );
+				local pPlot = Map.GetPlot(gridPosX,gridPosY);
+				FeatureType = pPlot:GetFeatureType();
+				print(GameInfo.Features.FEATURE_FOREST_CORRUPT.ID);
+				if FeatureType == FeatureTypes.NO_FEATURE then 
+					pPlot:SetFeatureType(GameInfo.Features.FEATURE_CORRUPTION.ID, -1);
+				elseif FeatureType == FeatureTypes.FEATURE_JUNGLE then
+					pPlot:SetFeatureType(GameInfo.Features.FEATURE_JUNGLE_CORRUPT.ID, -1);
+				elseif FeatureType == FeatureTypes.FEATURE_MARSH then
+					pPlot:SetFeatureType(GameInfo.Features.FEATURE_MARSH_CORRUPT.ID, -1);
+				elseif FeatureType == FeatureTypes.FEATURE_OASIS then
+					pPlot:SetFeatureType(GameInfo.Features.FEATURE_OASIS_CORRUPT.ID, -1);
+				elseif FeatureType == FeatureTypes.FEATURE_FLOOD_PLAINS then
+					pPlot:SetFeatureType(GameInfo.Features.FEATURE_FLOOD_PLAINS_CORRUPT.ID, -1);
+				elseif FeatureType == FeatureTypes.FEATURE_FOREST then
+					pPlot:SetFeatureType(GameInfo.Features.FEATURE_FOREST_CORRUPT.ID, -1);
+				elseif FeatureType == FeatureTypes.FEATURE_FALLOUT then
+				elseif FeatureType == FeatureTypes.FEATURE_ICE then
+				else
+				end
+			end
+		end
+	end
+end
+Events.SerialEventHexCultureChanged.Add(NecroCorruptionSpread)
